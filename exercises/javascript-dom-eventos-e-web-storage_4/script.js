1b1a98c5a08202dd5d0962f2f2d5488de2380200
Genivaldo <cgenivaldo46@gmail.com>
2021-04-21 05:14:44 -0300

    Gabarito mudando a cor da fonte

--- exercises/javascript-dom-eventos-e-web-storage_4/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_4/script.js
@@ -1,16 +1,20 @@
+// Botões cores de fundo
 window.onload = function() {
 	function mudaCorDeFundo(color) {
 		let content = document.querySelector('.content')
 		content.style.backgroundColor = color
 		localStorage.setItem('backgroundColor', color)
 	}	
-	
-
-
-
+  // Botões cores da fonte
+  function mudaCorDaFonte(color) {
+    let paragrafos = document.querySelectorAll('.paragraph')
+    for (let index = 0; index < paragrafos.length; index += 1) {
+      paragrafos[index].style.color = color
+    }
+    localStorage.setItem('font-color', color)
+  }
 
 // Alterando a cor de fundo
-
 let botaoCorDeFundo = document.querySelectorAll('#background-color>button') 
 for (let index = 0; index < botaoCorDeFundo.length; index += 1) {
 	 botaoCorDeFundo[index].addEventListener('click', function(event) {
@@ -18,11 +22,24 @@ for (let index = 0; index < botaoCorDeFundo.length; index += 1) {
 	 })
 }
 
+// Alterando a cor da fonte
+let fontesDoBotao = document.querySelectorAll("#font-color>button")
+for (let index = 0; index < fontesDoBotao.length; index += 1) {
+  fontesDoBotao[index].addEventListener('click', function(event) {
+    mudaCorDaFonte(event.target.innerHTML)
+  })
+}
 
+// Inicialização
 function inicializa () {
-	let corDeFundo = localStorage.getItem('background-color')
+	let corDeFundo = localStorage.getItem('backgroundColor')
 	if (corDeFundo) mudaCorDeFundo(corDeFundo)
+
+  let corDaFonte = localStorage.getItem('fontColor')
+  if (corDaFonte) mudaCorDaFonte(corDaFonte)
+
 }
 
 inicializa()
+
 }
