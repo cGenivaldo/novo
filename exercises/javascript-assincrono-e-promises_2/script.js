2a6c7e269c851e4b8621da2b4857d0db49fbb089
Genivaldo <cgenivaldo46@gmail.com>
2021-05-20 09:19:32 -0300

    Exercicio-03

--- exercises/javascript-assincrono-e-promises_2/script.js
+++ exercises/javascript-assincrono-e-promises_2/script.js
@@ -1,23 +1,18 @@
-// 2-Agora, um passo para trás: vamos fazer, passo a passo, uma Promise
-// Primeiramente, instancie uma Promise
-// Dentro dela, você deve produzir um array com dez números aleatórios de 1 a 50 e elevá-los todos ao quadrado.
-// Se a soma de todos esses elementos for inferior a 8000, a promise deve ser resolvida. Caso contrário, ela deve ser rejeitada. Acresça um then , com um console.log('Promise resolvida') e um catch , com um console.log('Promise rejeitada') à Promise , só para que o código funcione e possamos ver o resultado.
-// Tente usar Higher Order Functions! Lembre-se de que tanto uma quanto a outra recebem, como parâmetro, funções!
-
-const fetPromise = () => {
-    const myPromise = new  Promise((resolve, reject) => {
-       const myArray = Array.from(
-            { length: 10 },
-            () => Math.floor(Math.random() * 50) + 1,
-        )
-    const sum = myArray.map(number => number * number).reduce((sum, number) => sum + number)
+// Quando a promise for resolvida com sucesso, retorne um array com 4 itens, sendo eles o resultado da divisão do numero resultante por 2, 3, 5 e 10.
+const fetchPromise = () => {
+    const myPromise = new Promise((resolve, reject) => {
+      const myArray = Array.from(
+        { length: 10 },
+        () =>  Math.floor(Math.random() * 50) + 1
+      )
+   const sum = myArray.map(number => number * number).reduce((number, acc) => number + acc, 0)
         
-    sum < 8000 ? resolve(sum) : reject(sum)
-    })
+       (sum < 8000) ? resolve(sum) : reject()
+   })
 
     myPromise
-        .then((sum) => console.log(`Promisse resolvida ${sum}`))
-        .catch((sum) => console.log(`Promisse rejeitada ${sum}`))
+       .then(sum => [2, 3, 5, 10].map(number => sum / number))
+       .catch(() => console.log(`Promise rejeitada`))
 }
 
-fetPromise()
+fetchPromise()
